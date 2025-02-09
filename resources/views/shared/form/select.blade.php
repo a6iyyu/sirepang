20d0ceff0eb1987ced4a986f9116bdd8020c6617
finny <katthoriq@gmail.com>
2025-02-09 11:42:04 +0700

    tidy it up a bit

--- resources/views/shared/form/select.blade.php
+++ resources/views/shared/form/select.blade.php
@@ -13,8 +13,8 @@
                     <option value="{{ $value }}">{{ $label }}</option>
                 @endforeach
             @endif
-        </select>
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-5 mt-3 text-gray-700">
+            </select>
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-5 mt-3 text-gray-700">
             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                 <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
             </svg>
