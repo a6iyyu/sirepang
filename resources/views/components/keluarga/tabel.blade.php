e6f1ccc8d3eb46e20e667b18f532f3eea067fea6
finny <katthoriq@gmail.com>
2025-02-17 07:19:26 +0700

    fix table as per request of mr dika

--- resources/views/components/keluarga/tabel.blade.php
+++ resources/views/components/keluarga/tabel.blade.php
@@ -1,6 +1,28 @@
 <section class="mt-2 mb-8">
     @include('shared.table.table', [
-        'headers' => ['nama', 'desa', 'alamat', 'anggota', 'status', 'Fisiologi' , 'pangan', ],
-        'sortable' => ['desa', 'pangan'],
+        'headers' => ['nama', 'desa', 'aksi' ],
+        'sortable' => ['desa'],
     ])
+    @if(count($data) > 0)
+        <tr>
+            <th>nama</th>
+            <th>desa</th>
+            <th>Aksi</th>
+        </tr>
+        @foreach($data as $item)
+            <tr>
+                <td>{{ $item->nama }}</td>
+                <td>{{ $item->desa }}</td>
+                <td>
+                    <a href='{{ route("detail", $item->id) }}'>Detail</a>
+                    <a href='{{ route("edit", $item->id) }}'>Edit</a>
+                    <form action='{{ route("delete", $item->id) }}' method='POST' style='display:inline;'>
+                        @csrf
+                        @method('DELETE')
+                        <button type='submit'>Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    @endif
 </section>
