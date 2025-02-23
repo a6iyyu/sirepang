5b9cf0d6fc22eab8ff539736d7fe31c657dc5060
VozSoldat <muhamaderril7@gmail.com>
2025-02-23 17:19:16 +0700

    Add a simple element button

--- resources/views/testing/test.blade.php
+++ resources/views/testing/test.blade.php
@@ -1,3 +1,26 @@
+<script>
+    let index = 2; // Mulai dari 2 karena sudah ada dua kelompok
+
+    function addPangan() {
+        const container = document.getElementById("pangan-container");
+        const div = document.createElement("div");
+        div.classList.add("pangan-group");
+        div.setAttribute("data-index", index);
+        div.innerHTML = `
+            <p>Pangan ke-${index + 1}</p>
+            <input type="text" name="detail_pangan_keluarga[${index}][jumlah_urt]" placeholder="Jumlah URT">
+            <input type="text" name="detail_pangan_keluarga[${index}][id_keluarga]" placeholder="ID Keluarga">
+            <input type="text" name="detail_pangan_keluarga[${index}][id_pangan]" placeholder="ID Pangan">
+            <button type="button" onclick="removePangan(this)">Hapus</button>
+        `;
+        container.appendChild(div);
+        index++;
+    }
+
+    function removePangan(button) {
+        button.parentElement.remove();
+    }
+</script>
 <div>
     <!-- Life is available only in the present moment. - Thich Nhat Hanh -->
     <form action="{{ route('testing') }}" method="POST" enctype="multipart/form-data">
@@ -17,15 +40,21 @@
         <input type="text" name="rentang_pendapatan" value="2">
         <input type="text" name="rentang_pengeluaran" value="2">
 
-        {{-- pangan ke-1 --}}
-        <input type="text" name="detail_pangan_keluarga[0][jumlah_urt]" value="2">
-        <input type="text" name="detail_pangan_keluarga[0][id_keluarga]" value="2">
-        <input type="text" name="detail_pangan_keluarga[0][id_pangan]" value="2">
-
-        {{-- pangan ke-2 --}}
-        <input type="text" name="detail_pangan_keluarga[1][jumlah_urt]" value="7">
-        <input type="text" name="detail_pangan_keluarga[1][id_keluarga]" value="2">
-        <input type="text" name="detail_pangan_keluarga[1][id_pangan]" value="3">
+        <div id="pangan-container">
+            <div class="pangan-group" data-index="0">
+                <p>Pangan ke-1</p>
+                <input type="text" name="detail_pangan_keluarga[0][jumlah_urt]" value="2">
+                <input type="text" name="detail_pangan_keluarga[0][id_keluarga]" value="2">
+                <input type="text" name="detail_pangan_keluarga[0][id_pangan]" value="2">
+            </div>
+            <div class="pangan-group" data-index="1">
+                <p>Pangan ke-2</p>
+                <input type="text" name="detail_pangan_keluarga[1][jumlah_urt]" value="7">
+                <input type="text" name="detail_pangan_keluarga[1][id_keluarga]" value="2">
+                <input type="text" name="detail_pangan_keluarga[1][id_pangan]" value="3">
+            </div>
+        </div>
+        <button type="button" onclick="addPangan()">Tambah Pangan</button>
 
         <button type="submit">Submit</button>
     </form>
