e1bcb49e65b8fbfb11179b6cb6742e8f4e8d4f2c
a6iyyu <mizukinako7@gmail.com>
2025-04-25 07:22:53 +0700

    Mengganti nama kecamatan ke nama pengguna di halaman login.

--- resources/views/pages/admin/rekap-kecamatan.blade.php
+++ resources/views/pages/admin/rekap-kecamatan.blade.php
@@ -23,7 +23,7 @@
         <div class=" mt-10 flex flex-col items-start w-96 h-auto p-6 rounded-xl shadow-lg bg-green-dark border-2 border-white text-white hover:shadow-2xl transition-shadow duration-300 lg:mr-10">
             @foreach ($tahun as $thn)
             <h2 class="text-2xl font-bold mb-4">Rekap {{ $thn }}</h2>
-            <a href="{{ url('admin/data-kecamatan/rekap-kecamatan/tahun/' . $thn) }}"
+            <a href="{{ url("admin/data-kecamatan/rekap-kecamatan/tahun/$thn") }}"
                class="bg-primary text-primary text-lg font-semibold hover:bg-green-600 hover:scale-105 transform transition-all duration-200 rounded-md px-4 py-2 my-1 w-full text-center">
                 <i class="fa-solid fa-file-excel mr-2"></i>
             Unduh
