594626a85a9e115a255f5f79330e3b5ac7cda2fb
Yeon <giojio825@gmail.com>
2025-06-28 22:07:02 +0700

    :wrench: Enhance export_rekap method to include total consumption calculations and improve spreadsheet formatting

--- app/Http/Controllers/Kecamatan.php
+++ app/Http/Controllers/Kecamatan.php
@@ -10,6 +10,7 @@ use App\Models\JenisPangan as JenisPanganModel;
 use App\Models\Kecamatan as KecamatanModel;
 use App\Models\Pangan as PanganModel;
 use App\Models\Keluarga as KeluargaModel;
+use App\Models\PanganKeluarga as PanganKeluargaModel;
 use Illuminate\View\View;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
@@ -48,6 +49,21 @@ class Kecamatan extends Controller
 
     public function export_rekap($id, $th)
     {
+        $kecamatan = KecamatanModel::find($id);
+        // dd($kecamatan);
+        $kk = KeluargaModel::where('id_kecamatan', $id)
+            ->whereYear('created_date', $th)
+            ->sum('jumlah_keluarga');
+
+        $year = (int) $th;
+        $isLeapYear = date('L', mktime(0, 0, 0, 1, 1, $year));
+        $daysInYear = $isLeapYear ? 366 : 365;
+
+        $currentYear = (int) date('Y');
+        if ($year == $currentYear) {
+            $daysInYear = date('z') + 1; // jumlah hari 'jika' tahun ini
+        }
+
         $jenis_pangan = JenisPanganModel::select('id_jenis_pangan', 'nama_jenis', 'parent')->where('parent', "=", null)->get()->toArray();
         $sub_jenis_pangan = JenisPanganModel::select('id_jenis_pangan', 'nama_jenis', 'parent')->where('parent', "!=", null)->get()->toArray();
         $pangan = PanganModel::with('jenis_pangan')->get()->toArray();
@@ -63,6 +79,7 @@ class Kecamatan extends Controller
                 'protein' => $data['protein'],
                 'lemak' => $data['lemak'],
                 'karbohidrat' => $data['karbohidrat'],
+                'referensi_urt' => $data['referensi_urt'],
             ];
         });
 
@@ -74,65 +91,131 @@ class Kecamatan extends Controller
         $sheet->getStyle("B1")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
         $sheet->setCellValue("C1", "DBKM SUSENAS")->mergeCells("C1:G1");
         $sheet->getStyle("C1")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->setCellValue("H1", "Rata - Rata Konsumsi per Kapita / Minggu")->mergeCells("H1:K1");
+        $sheet->getStyle("H1")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
         $sheet->setCellValue("C3", "Berat (gr)");
         $sheet->setCellValue("D3", "Energi (kkal)");
         $sheet->setCellValue("E3", "Protein (gr)");
         $sheet->setCellValue("F3", "Lemak (gr)");
         $sheet->setCellValue("G3", "Karbo (gr)");
+        $sheet->setCellValue("H2", "Wilayah")->mergeCells("H2:K2");
+        $sheet->getStyle("H2")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->setCellValue("H3", "Satuan");
+        $sheet->setCellValue("I3", "Jumlah");
+        $sheet->setCellValue("J3", "7 Hari");
+        $sheet->setCellValue("K3", "Total");
+        $sheet->setCellValue("L3", "kk");
 
         $row = 4;
         $index = 0;
         $num = 1;
-        $ber = 1;
         foreach ($jenis_pangan as $key_jenis => $value_jenis_pangan) {
             $sheet->setCellValue("B{$row}", $num .  ". " . $value_jenis_pangan['nama_jenis']);
             $row++;
 
             while ($pangan_list[$index]['id_jenis_pangan'] == $value_jenis_pangan['id_jenis_pangan'] && $index < 177) {
+                $totalKonsumsi = PanganKeluargaModel::whereHas('keluarga', function ($query) use ($id, $th) {
+                    $query->where('id_kecamatan', $id)
+                        ->whereYear('created_date', $th);
+                })->where('id_pangan', $pangan_list[$index]['id_pangan'])
+                    ->sum('urt');
+
+                $jumlah = $daysInYear > 0 ? $totalKonsumsi / $daysInYear : 0;
+
+                $total = $kk > 0 ? ($jumlah / $kk) * 7 : 0;
+
                 $sheet->setCellValue("B{$row}", $pangan_list[$index]['nama_pangan']);
                 $sheet->setCellValue("C{$row}", $pangan_list[$index]['berat']);
                 $sheet->setCellValue("D{$row}", $pangan_list[$index]['energi']);
                 $sheet->setCellValue("E{$row}", $pangan_list[$index]['protein']);
                 $sheet->setCellValue("F{$row}", $pangan_list[$index]['lemak']);
                 $sheet->setCellValue("G{$row}", $pangan_list[$index]['karbohidrat']);
+                $sheet->setCellValue("H{$row}", $pangan_list[$index]['referensi_urt']);
+                $sheet->setCellValue("I{$row}", $jumlah);
+                $sheet->setCellValue("J{$row}", 7);
+                $sheet->setCellValue("K{$row}", $total);
+                $sheet->setCellValue("L{$row}", $kk);
+
                 $sheet->getColumnDimension('B')->setWidth(40);
                 $sheet->getColumnDimension('C')->setAutoSize(true);
                 $sheet->getColumnDimension('D')->setAutoSize(true);
                 $sheet->getColumnDimension('E')->setAutoSize(true);
                 $sheet->getColumnDimension('F')->setAutoSize(true);
                 $sheet->getColumnDimension('G')->setAutoSize(true);
+                $sheet->getColumnDimension('H')->setAutoSize(true);
+                $sheet->getColumnDimension('I')->setAutoSize(true);
+                $sheet->getColumnDimension('J')->setAutoSize(true);
+                $sheet->getColumnDimension('K')->setAutoSize(true);
+                $sheet->getColumnDimension('L')->setAutoSize(true);
 
-
-                $columns = ['C', 'D', 'E', 'F', 'G'];
-                foreach ($columns as $col) $sheet->getStyle("{$col}{$row}")->getNumberFormat()->setFormatCode('#,##0.00');
+                $columns = ['C', 'D', 'E', 'F', 'G', 'I', 'K'];
+                foreach ($columns as $col)
+                    if ($col === 'K') {
+                                $sheet->getStyle("{$col}{$row}")
+                                    ->getNumberFormat()
+                                    ->setFormatCode('#,##0.000');
+                            } else {
+                                $sheet->getStyle("{$col}{$row}")
+                                    ->getNumberFormat()
+                                    ->setFormatCode('#,##0.00');
+                            }
                 $row++;
                 $index++;
             }
 
+            $ber = 1;
             foreach ($sub_jenis_pangan as $key_sub => $value_sub_jenis_pangan) {
                 if ($value_jenis_pangan['id_jenis_pangan'] == $sub_jenis_pangan[$key_sub]['parent'] && $index < 177) {
                     $sheet->setCellValue("B{$row}", $num . "." . $ber . $value_sub_jenis_pangan['nama_jenis']);
                     $row++;
+                    $ber++;
 
                     while ($pangan_list[$index]['id_jenis_pangan'] == $value_sub_jenis_pangan['id_jenis_pangan']) {
+                        $totalKonsumsi = PanganKeluargaModel::whereHas('keluarga', function ($query) use ($id, $th) {
+                            $query->where('id_kecamatan', $id)
+                                ->whereYear('created_date', $th);
+                        })->where('id_pangan', $pangan_list[$index]['id_pangan'])
+                            ->sum('urt');
+
+                        $jumlah = $daysInYear > 0 ? $totalKonsumsi / $daysInYear : 0;
+
+                        $total = $kk > 0 ? ($jumlah / $kk) * 7 : 0;
+
                         $sheet->setCellValue("B{$row}", $pangan_list[$index]['nama_pangan']);
                         $sheet->setCellValue("C{$row}", $pangan_list[$index]['berat']);
                         $sheet->setCellValue("D{$row}", $pangan_list[$index]['energi']);
                         $sheet->setCellValue("E{$row}", $pangan_list[$index]['protein']);
                         $sheet->setCellValue("F{$row}", $pangan_list[$index]['lemak']);
                         $sheet->setCellValue("G{$row}", $pangan_list[$index]['karbohidrat']);
+                        $sheet->setCellValue("H{$row}", $pangan_list[$index]['referensi_urt']);
+                        $sheet->setCellValue("I{$row}", $jumlah);
+                        $sheet->setCellValue("J{$row}", 7);
+                        $sheet->setCellValue("K{$row}", $total);
+                        $sheet->setCellValue("L{$row}", $kk);
+
                         $sheet->getColumnDimension('B')->setWidth(40);
                         $sheet->getColumnDimension('C')->setAutoSize(true);
                         $sheet->getColumnDimension('D')->setAutoSize(true);
                         $sheet->getColumnDimension('E')->setAutoSize(true);
                         $sheet->getColumnDimension('F')->setAutoSize(true);
                         $sheet->getColumnDimension('G')->setAutoSize(true);
+                        $sheet->getColumnDimension('H')->setAutoSize(true);
+                        $sheet->getColumnDimension('I')->setAutoSize(true);
+                        $sheet->getColumnDimension('J')->setAutoSize(true);
+                        $sheet->getColumnDimension('K')->setAutoSize(true);
+                        $sheet->getColumnDimension('L')->setAutoSize(true);
 
-                        $columns = ['C', 'D', 'E', 'F', 'G'];
+                        $columns = ['C', 'D', 'E', 'F', 'G', 'I', 'K'];
                         foreach ($columns as $col) {
-                            $sheet->getStyle("{$col}{$row}")
-                                ->getNumberFormat()
-                                ->setFormatCode('#,##0.00');
+                            if ($col === 'K') {
+                                $sheet->getStyle("{$col}{$row}")
+                                    ->getNumberFormat()
+                                    ->setFormatCode('#,##0.000');
+                            } else {
+                                $sheet->getStyle("{$col}{$row}")
+                                    ->getNumberFormat()
+                                    ->setFormatCode('#,##0.00');
+                            }
                         }
                         $row++;
                         $index++;
@@ -142,10 +225,8 @@ class Kecamatan extends Controller
             $num++;
         }
 
-        $kecamatan = KecamatanModel::find($id)->select('nama_kecamatan')->first();
-
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment; filename="' . 'Data_Rekap_' . $kecamatan->nama_kecamatan .'_'. date('Y-m-d_H-i-s') . '.xlsx' . '"');
+        header('Content-Disposition: attachment; filename="' . 'Data_Rekap_' . $kecamatan->nama_kecamatan . '_' . date('Y-m-d_H-i-s') . '.xlsx' . '"');
         header('Cache-Control: max-age=0');
         header('Cache-Control: max-age=1');
         header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
